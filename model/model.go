046a3b43a26befd3b089b2b57ed930abc5c51f54
Your Name <you@example.com>
2022-08-24 09:35:13 +0700

    first commit

---
+++ model/model.go
@@ -0,0 +1,12 @@
+/* model.go */
+package model
+
+import "gorm.io/gorm"
+
+type Employee struct {
+	gorm.Model
+	Employee_id       string
+	Employee_name     string
+	Employee_username string
+	Employee_password string
+}
